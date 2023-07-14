ae14853abd33c51031f58e84124a072f4539c1bd
DanielPrawin <44159183+DanielPrawin@users.noreply.github.com>
2023-07-14 16:03:08 +0530

    Create math.groovy
    math

---
+++ vars/math.groovy
@@ -0,0 +1,17 @@
+Libraries
+def add(a,b)
+{
+  echo "The addition of ${a} and ${b} is ${a+b}"
+}
+def sub(a,b)
+{
+  echo "The subtraction of ${a} and ${b} is ${a-b}"
+}
+def mul(a,b)
+{
+  echo "The multiplication of ${a} and ${b} is ${a*b}"
+}
+def div(a,b)
+{
+  echo "The division of ${a} and ${b} is ${a/b}"
+}
